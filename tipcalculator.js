7834c11a1d4ae4a6063584caba9718cf4b61c4df
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-07-10 13:45:01 +0530

    html file added

--- tipcalculator.js
+++ tipcalculator.js
@@ -1,7 +1,1 @@
-function tipCalc(price, tip) {
-  return price + (tip / 100) * price;
-}
-const priceofFood = 450;
-const tipPercentage = 20;
-const finalAmount = tipCalc(priceofFood, tipPercentage);
-console.log("Total Amount is $" + Math.floor(finalAmount));
+
